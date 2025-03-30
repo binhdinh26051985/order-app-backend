cb266453c180866737262db6852cd948b3fec867
binhdinh26051985 <112796138+binhdinh26051985@users.noreply.github.com>
2025-03-30 11:46:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -134,7 +134,8 @@ app.use((err, req, res, next) => {
 // JWT authentication middleware
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
-  const token = authHeader?.split(' ')[1];
+  //const token = authHeader?.split(' ')[1];
+  const token = authHeader && authHeader.split(' ')[1];
   
   if (!token) return res.status(401).json({ error: 'Authorization token required' });
 
