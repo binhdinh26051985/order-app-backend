f5450433280b71b2dd7da0b5fe6cc2c336a6a8db
binhdinh26051985 <112796138+binhdinh26051985@users.noreply.github.com>
2025-03-31 13:14:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -35,8 +35,7 @@ const app = express();
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  exposedHeaders: ['Authorization']
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 }));
 
 
